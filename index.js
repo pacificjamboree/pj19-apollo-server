51f263edb8cb7d14aa58d56bff585fd0920e142b
Graham Ballantyne <grahamb@sfu.ca>
2018-01-14 00:02:13 -0800

    Add initial test GraphQL schema

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const knex = require('knex')(require('./knexfile')[process.env.NODE_ENV]);
+
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { OOS: [OOS] }
+  type OOS { 
+    oos_number: String!,
+    first_name: String!,
+    last_name: String!,
+    preferred_name: String!
+   }
+`;
 
-app.get('*', (req, res) => {
-  res.send('Hello world!');
+// The resolvers
+const resolvers = {
+  Query: {
+    OOS: () =>
+      knex
+        .from('oos')
+        .select(['oos_number', 'first_name', 'last_name', 'preferred_name'])
+  }
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
 
+const app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}.`);
 });
