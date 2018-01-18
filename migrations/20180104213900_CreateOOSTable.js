8ed172e0a71cb6696ac125165ac44c2d37eed46b
Graham Ballantyne <grahamb@sfu.ca>
2018-01-17 19:58:42 -0800

    Add updated_at timestamp triggers to tables

--- migrations/20180104213900_CreateOOSTable.js
+++ migrations/20180104213900_CreateOOSTable.js
@@ -1,37 +1,42 @@
+const table = 'oos';
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('oos', t => {
-    t
-      .uuid('id')
-      .primary()
-      .default(knex.raw('gen_random_uuid()'));
-    t.timestamps();
+  return knex.schema
+    .createTable(table, t => {
+      t
+        .uuid('id')
+        .primary()
+        .default(knex.raw('gen_random_uuid()'));
+      t.timestamps(true, true);
 
-    t.string('oos_number').notNullable();
-    t
-      .boolean('assigned')
-      .notNullable()
-      .defaultTo(false);
+      t.string('oos_number').notNullable();
+      t
+        .boolean('assigned')
+        .notNullable()
+        .defaultTo(false);
 
-    t.string('first_name').notNullable();
-    t.string('last_name').notNullable();
-    t.string('preferred_name');
-    t.date('birthdate');
-    t.boolean('is_youth');
+      t.string('first_name').notNullable();
+      t.string('last_name').notNullable();
+      t.string('preferred_name');
+      t.date('birthdate');
+      t.boolean('is_youth');
 
-    t.string('email');
-    t.string('phone1');
-    t.string('phone2');
+      t.string('email');
+      t.string('phone1');
+      t.string('phone2');
 
-    t.boolean('prerecruited').defaultTo(false);
-    t.string('prerecruited_by');
-    t.string('additional_information');
-    t.string('previous_experience');
-    t.string('special_skills');
+      t.boolean('prerecruited').defaultTo(false);
+      t.string('prerecruited_by');
+      t.string('additional_information');
+      t.string('previous_experience');
+      t.string('special_skills');
 
-    t.string('registration_status');
-  });
+      t.string('registration_status');
+    })
+    .raw(
+      `CREATE TRIGGER update_${table}_updated_at BEFORE UPDATE ON ${table} FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();`
+    );
 };
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('oos');
+  return knex.schema.dropTable(table);
 };
--- migrations/20180104215910_CreateAdventureTable.js
+++ migrations/20180104215910_CreateAdventureTable.js
@@ -1,35 +1,40 @@
+const table = 'adventure';
 exports.up = (knex, Promise) => {
-  return knex.schema.createTable('adventure', t => {
-    t
-      .uuid('id')
-      .primary()
-      .default(knex)
-      .default(knex.raw('gen_random_uuid()'));
-    t.timestamps();
+  return knex.schema
+    .createTable(table, t => {
+      t
+        .uuid('id')
+        .primary()
+        .default(knex)
+        .default(knex.raw('gen_random_uuid()'));
+      t.timestamps(true, true);
 
-    t.string('adventure_code');
-    t.string('name').notNullable();
-    t.string('theme_name').notNullable();
-    t.string('description');
-    t.enu('location', ['onsite', 'offsite']);
-    t.integer('capacity_per_period').notNullable();
-    t.integer('periods_offered').notNullable();
-    t.integer('periods_required').notNullable();
-    t
-      .boolean('premium_adventure')
-      .defaultTo(false)
-      .notNullable();
-    t
-      .decimal('fee')
-      .notNullable()
-      .defaultTo(0.0);
-    t
-      .boolean('hidden')
-      .notNullable()
-      .defaultTo(false);
-  });
+      t.string('adventure_code');
+      t.string('name').notNullable();
+      t.string('theme_name').notNullable();
+      t.string('description');
+      t.enu('location', ['onsite', 'offsite']);
+      t.integer('capacity_per_period').notNullable();
+      t.integer('periods_offered').notNullable();
+      t.integer('periods_required').notNullable();
+      t
+        .boolean('premium_adventure')
+        .defaultTo(false)
+        .notNullable();
+      t
+        .decimal('fee')
+        .notNullable()
+        .defaultTo(0.0);
+      t
+        .boolean('hidden')
+        .notNullable()
+        .defaultTo(false);
+    })
+    .raw(
+      `CREATE TRIGGER update_${table}_updated_at BEFORE UPDATE ON ${table} FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();`
+    );
 };
 
 exports.down = (knex, Promise) => {
-  return knex.schema.dropTable('adventure');
+  return knex.schema.dropTable(table);
 };
