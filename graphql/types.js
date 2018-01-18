04aae9e8c121fec35efcdd9b9a919d095ed757ae
Graham Ballantyne <grahamb@sfu.ca>
2018-01-17 22:08:47 -0800

    Add remaining fields to OOS type

--- graphql/types.js
+++ graphql/types.js
@@ -1,14 +1,32 @@
 const types = `
+  scalar GraphQLDate
+  scalar GraphQLDateTime
+
   type Query { 
     allOffersOfService: [OOS],
     offerOfService(oos_number: String!): OOS
   }
 
   type OOS { 
+    id: ID!,
     oos_number: String!,
+    assigned: Boolean!,
     first_name: String!,
     last_name: String!,
-    preferred_name: String
+    preferred_name: String,
+    birthdate: GraphQLDate!,
+    is_youth: Boolean!,
+    email: String,
+    phone1: String,
+    phone2: String,
+    prerecruited: Boolean!,
+    prerecruited_by: String,
+    additional_information: String,
+    previous_experience: String,
+    special_skills: String,
+    registration_status: String,
+    created_at: GraphQLDateTime!,
+    updated_at: GraphQLDateTime!
    }
 `;
 
