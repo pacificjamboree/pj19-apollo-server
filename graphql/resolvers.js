8554a095827a19b1eaaa0fcb3c1d237986e6f05d
Graham Ballantyne <grahamb@sfu.ca>
2018-01-17 22:05:54 -0800

    Select all fields for OOS

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,14 +2,11 @@ const knex = require('knex')(require('../knexfile')[process.env.NODE_ENV]);
 
 const resolvers = {
   Query: {
-    allOffersOfService: () =>
-      knex
-        .from('oos')
-        .select(['oos_number', 'first_name', 'last_name', 'preferred_name']),
+    allOffersOfService: () => knex.from('oos').select('*'),
     offerOfService: (_, { oos_number }) => {
       return knex
         .from('oos')
-        .select(['oos_number', 'first_name', 'last_name', 'preferred_name'])
+        .select('*')
         .where({ oos_number: oos_number })
         .first();
     }
