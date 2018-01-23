921defc6feda9210c0e8b0bbabe9efc5cc763a8e
Graham Ballantyne <grahamb@sfu.ca>
2018-01-23 11:32:05 -0800

    Create seed for fake adventures

---
+++ seeds/development/0002_ FakeAdventure.js
@@ -0,0 +1,75 @@
+const table = 'adventure';
+
+const data = [
+  {
+    adventure_code: 'ADV001',
+    name: 'SUP',
+    theme_name: 'SUP',
+    description: 'SUP',
+    location: 'onsite',
+    capacity_per_period: 50,
+    periods_offered: 11,
+    periods_required: 1,
+    premium_adventure: false,
+    fee: 0.0,
+    hidden: false
+  },
+  {
+    adventure_code: 'ADV002',
+    name: 'Kayaking',
+    theme_name: 'Kayaking',
+    description: 'Kayaking',
+    location: 'offsite',
+    capacity_per_period: 36,
+    periods_offered: 11,
+    periods_required: 1,
+    premium_adventure: true,
+    fee: 0.0,
+    hidden: false
+  },
+  {
+    adventure_code: 'ADV003',
+    name: 'SCUBA',
+    theme_name: 'SCUBA',
+    description: 'SCUBA',
+    location: 'offsite',
+    capacity_per_period: 50,
+    periods_offered: 10,
+    periods_required: 1,
+    premium_adventure: false,
+    fee: 50.0,
+    hidden: false
+  },
+  {
+    adventure_code: 'ADV004',
+    name: 'Townsite',
+    theme_name: 'Townsite',
+    description: 'Townsite',
+    location: 'onsite',
+    capacity_per_period: 200,
+    periods_offered: 11,
+    periods_required: 1,
+    premium_adventure: false,
+    fee: 0.0,
+    hidden: false
+  },
+  {
+    adventure_code: 'ADV005',
+    name: 'Mtn. Biking',
+    theme_name: 'Mtn. Biking',
+    description: 'Mtn. Biking',
+    location: 'onsite',
+    capacity_per_period: 50,
+    periods_offered: 11,
+    periods_required: 1,
+    premium_adventure: true,
+    fee: 0.0,
+    hidden: false
+  }
+];
+
+exports.seed = function(knex, Promise) {
+  return knex(table)
+    .del()
+    .then(() => knex(table).insert(data));
+};
