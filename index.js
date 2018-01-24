61705ff8c0ee0dec8abce00a4219cc774f44c9ea
Graham Ballantyne <grahamb@sfu.ca>
2018-01-23 16:17:16 -0800

    Disable engine for testing

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { Engine } = require('apollo-engine');
+// const { Engine } = require('apollo-engine');
 const compression = require('compression');
 const schema = require('./graphql/schema');
 const PORT = process.env.PORT || 3000;
-const { APOLLO_ENGINE_API_KEY } = process.env;
-const engine = new Engine({
-  engineConfig: {
-    apiKey: APOLLO_ENGINE_API_KEY
-  },
-  graphqlPort: PORT
-});
-engine.start();
+// const { APOLLO_ENGINE_API_KEY } = process.env;
+// const engine = new Engine({
+//   engineConfig: {
+//     apiKey: APOLLO_ENGINE_API_KEY
+//   },
+//   graphqlPort: PORT
+// });
+// engine.start();
 
 const app = express();
-app.use(engine.expressMiddleware());
+// app.use(engine.expressMiddleware());
 app.use(compression());
-app.use(
-  '/graphql',
-  bodyParser.json(),
-  graphqlExpress({ schema, tracing: true })
-);
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}.`);
