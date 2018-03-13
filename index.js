1af43d5e3a3e81750b0549d93ba0fbfb6e4b84f2
Graham Ballantyne <grahamb@sfu.ca>
2018-03-13 14:56:07 -0700

    Remove apollo-engine

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-// const { Engine } = require('apollo-engine');
 const compression = require('compression');
 const schema = require('./graphql/schema');
 const PORT = process.env.PORT || 3000;
-// const { APOLLO_ENGINE_API_KEY } = process.env;
-// const engine = new Engine({
-//   engineConfig: {
-//     apiKey: APOLLO_ENGINE_API_KEY
-//   },
-//   graphqlPort: PORT
-// });
-// engine.start();
 
 const app = express();
 // app.use(engine.expressMiddleware());
