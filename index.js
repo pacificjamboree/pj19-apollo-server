d24e1eb41b76d082766d51dad217e50a5076eb3e
Graham Ballantyne <grahamb@sfu.ca>
2018-03-13 16:06:15 -0700

    Add login route

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const compression = require('compression');
 const schema = require('./graphql/schema');
 const PORT = process.env.PORT || 3000;
 
+const { login } = require('./app/routes');
+
 const app = express();
-// app.use(engine.expressMiddleware());
 app.use(compression());
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+app.post('/login', bodyParser.urlencoded({ extended: false }), login);
+app.post('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}.`);
 });
