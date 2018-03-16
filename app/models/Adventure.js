9f148c3b5d13ff85e96aaec5e20a5c82caaabb9f
Graham Ballantyne <grahamb@sfu.ca>
2018-03-16 15:49:27 -0700

    Add initial Adventure model

---
+++ app/models/Adventure.js
@@ -0,0 +1,32 @@
+const Model = require('./BaseModel');
+
+class Adventure extends Model {
+  static get tableName() {
+    return 'adventure';
+  }
+
+  static get relationMappings() {
+    const OfferOfService = require('./OfferOfService');
+    return {
+      offersOfService: {
+        relation: Model.HasManyRelation,
+        modelClass: OfferOfService,
+        join: { from: 'adventure.id', to: 'oos.assignedAdventureId' },
+      },
+      managers: {
+        relation: Model.ManyToManyRelation,
+        modelClass: OfferOfService,
+        join: {
+          from: 'adventure.id',
+          through: {
+            from: 'adventure_manager.adventure_id',
+            to: 'adventure_manager.oos_id',
+          },
+          to: 'oos.id',
+        },
+      },
+    };
+  }
+}
+
+module.exports = Adventure;
