34bc8cac74d2028ff778bd22d7ba05a62ce25753
Graham Ballantyne <grahamb@sfu.ca>
2018-03-16 15:49:45 -0700

    Flesh out OfferOfService model

--- app/models/OfferOfService.js
+++ app/models/OfferOfService.js
@@ -1,12 +1,28 @@
+const differenceInYears = require('date-fns/difference_in_years');
 const Model = require('./BaseModel');
 
 class OfferOfService extends Model {
-  static getTableName() {
+  static get tableName() {
     return 'oos';
+  }
+
+  static get relationMappings() {
+    const Adventure = require('./Adventure');
+    return {
+      assignment: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Adventure,
+        join: { from: 'oos.assignedAdventureId', to: 'adventure.id' },
+      },
+    };
   }
 
   fullName() {
     return `${this.firstName} ${this.lastName}`;
+  }
+
+  isYouth() {
+    return differenceInYears(new Date(), this.birthdate) <= 18;
   }
 }
 
