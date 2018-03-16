2a0be4211eed20b6122aff4f4a595424774e35ea
Graham Ballantyne <grahamb@sfu.ca>
2018-03-16 16:32:32 -0700

    Add fullName method to PatrolScouter

--- app/models/PatrolScouter.js
+++ app/models/PatrolScouter.js
@@ -4,6 +4,10 @@ class PatrolScouter extends Model {
   static getTableName() {
     return 'patrol_scouter';
   }
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
 
 module.exports = PatrolScouter;
