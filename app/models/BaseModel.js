87ba563dad56e21faca0fe940209854dfbf36bee
Graham Ballantyne <grahamb@sfu.ca>
2018-03-21 16:23:51 -0700

    Add a globalId() method to BaseModel
    Every model now has a built in way to get a relay-compatible global ID

--- app/models/BaseModel.js
+++ app/models/BaseModel.js
@@ -1,5 +1,16 @@
 const { Model } = require('objection');
 const db = require('../../db');
+const { toGlobalId } = require('graphql-relay-tools');
+
 Model.knex(db);
 
-module.exports = Model;
+class BaseModel extends Model {
+  constructor() {
+    super();
+  }
+
+  globalId() {
+    return toGlobalId(this.constructor.name, this.id);
+  }
+}
+module.exports = BaseModel;
