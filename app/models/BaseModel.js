52e9ae9ab234805f154e0e085010160cffe9530a
Graham Ballantyne <grahamb@sfu.ca>
2018-03-21 17:18:33 -0700

    Remove useless super call in BaseModel

--- app/models/BaseModel.js
+++ app/models/BaseModel.js
@@ -5,10 +5,6 @@ const { toGlobalId } = require('graphql-relay-tools');
 Model.knex(db);
 
 class BaseModel extends Model {
-  constructor() {
-    super();
-  }
-
   globalId() {
     return toGlobalId(this.constructor.name, this.id);
   }
