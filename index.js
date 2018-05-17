a3a1a24317aa24099b03fff956c26f15af669baa
Graham Ballantyne <grahamb@sfu.ca>
2018-05-17 00:37:00 -0700

    Add jwt-express middleware
    # Conflicts:
    #	index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const compression = require('compression');
+const jwtMiddleware = require('express-jwt');
 const schema = require('./app/graphql/schema');
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +12,12 @@ const app = express();
 app.use(compression());
 
 app.post('/login', bodyParser.urlencoded({ extended: false }), login);
-app.post('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.post(
+  '/graphql',
+  bodyParser.json(),
+  jwtMiddleware({ credentialsRequired: false, secret: process.env.JWT_SECRET }),
+  graphqlExpress({ schema })
+);
 app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}.`);
