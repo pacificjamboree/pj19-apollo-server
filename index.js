cf103d550f026da95d7f7a1751815adcbcd71ec4
Graham Ballantyne <grahamb@sfu.ca>
2018-05-23 15:46:20 -0700

    Refactor graphqlExpress middleware, add middleware to inject roles
    TODO refactor the roles middleware further

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const compression = require('compression');
 const jwtMiddleware = require('express-jwt');
 const schema = require('./app/graphql/schema');
 const PORT = process.env.PORT || 3000;
-
+const { User } = require('./app/models');
 const { login } = require('./app/routes');
 
 const app = express();
@@ -15,8 +15,40 @@ app.post('/login', bodyParser.urlencoded({ extended: false }), login);
 app.post(
   '/graphql',
   bodyParser.json(),
-  jwtMiddleware({ credentialsRequired: false, secret: process.env.JWT_SECRET }),
-  graphqlExpress({ schema })
+  jwtMiddleware({
+    credentialsRequired: false,
+    secret: process.env.JWT_SECRET,
+    requestProperty: 'auth',
+  }),
+  async (req, res, next) => {
+    if (req.auth === undefined) next();
+    const user = await User.query()
+      .where({ id: req.auth.sub.id })
+      .select([
+        'id',
+        'oosId',
+        'patrolScouterId',
+        'username',
+        'workflowState',
+        'admin',
+      ])
+      .first();
+    const roles = await user.calculateRoles();
+    req.user = {
+      ...user,
+      roles,
+    };
+    next();
+  },
+  graphqlExpress(req => {
+    return {
+      schema,
+      context: {
+        auth: req.auth,
+        user: req.user,
+      },
+    };
+  })
 );
 app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 app.listen(PORT, () => {
