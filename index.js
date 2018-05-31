8a108165c38304f6f8ccf6c23e9992a37b3863e6
Graham Ballantyne <grahamb@sfu.ca>
2018-05-31 14:17:19 -0700

    Add CORS headers

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const { login } = require('./app/routes');
 const app = express();
 app.use(compression());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.post('/login', bodyParser.urlencoded({ extended: false }), login);
 app.post(
   '/graphql',
