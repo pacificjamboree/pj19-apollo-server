fa79ac28e7a1394d3db39fdaa65e13324c9d0923
Graham Ballantyne <grahamb@sfu.ca>
2018-11-23 20:53:24 -0800

    Specify AWS region in after-install script

--- scripts/codedeploy/after-install.sh
+++ scripts/codedeploy/after-install.sh
@@ -3,7 +3,7 @@
 # get params from ssm and write to file
 # install fx
 if [ ! -L /usr/local/bin/fx ] ; then npm -g i fx ; fi
-aws ssm get-parameters-by-path --path /graphql/prod/env/ --output json | \
+aws ssm get-parameters-by-path --path /graphql/prod/env/ --output json --region ca-central-1 | \
   fx 'this.Parameters.map(x => `${x.Name.replace(/\/graphql\/prod\/env\//, "")}=${x.Value}`).join("\n")' \
   > /home/ec2-user/server/.env
 
