423bd5902334aef90b76ba8d049883ffaa00fe0c
Graham Ballantyne <grahamb@sfu.ca>
2019-03-11 11:41:35 -0700

    Add path to codedeploy scripts

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/bin:$PATH
 
 su -c "pm2 start jobs" ec2-user
 service httpd start
--- scripts/codedeploy/application-stop.sh
+++ scripts/codedeploy/application-stop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PATH=/usr/local/bin:$PATH
 
 su -c "pm2 stop jobs" ec2-user
 service httpd stop
