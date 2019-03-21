1725384b9f68ef7931771713270ebcfe3478865e
Graham Ballantyne <grahamb@sfu.ca>
2019-03-20 17:13:21 -0700

    Add relationship to User from PatrolScouter

--- app/models/PatrolScouter.js
+++ app/models/PatrolScouter.js
@@ -7,11 +7,17 @@ class PatrolScouter extends Model {
 
   static get relationMappings() {
     const Patrol = require('./Patrol');
+    const User = require('./User');
     return {
       patrols: {
         relation: Model.HasManyRelation,
         modelClass: Patrol,
         join: { from: 'patrol_scouter.id', to: 'patrol.patrolScouterId' },
+      },
+      user: {
+        relation: Model.HasOneRelation,
+        modelClass: User,
+        join: { from: 'patrol_scouter.id', to: 'patrolScouterId.id' },
       },
     };
   }
