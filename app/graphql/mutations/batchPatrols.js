0082d2944f89666706774e0f3bb3670ba78f3296
Graham Ballantyne <grahamb@sfu.ca>
2019-03-20 17:32:47 -0700

    Add mutation to deal with a batch of patrols

---
+++ app/graphql/mutations/batchPatrols.js
@@ -0,0 +1,19 @@
+const { mutationWithClientMutationId } = require('graphql-relay-tools');
+const { batchPatrols } = require('../resolvers/patrol');
+
+module.exports = mutationWithClientMutationId({
+  name: 'batchPatrols',
+  inputFields: `
+    ImportPatrols: [PatrolImportDraft]!
+    DeletePatrols: [ID]!
+    `,
+  outputFields: `
+    ImportedPatrols: [Patrol]
+    DeletedPatrols: [Patrol]
+  `,
+  mutateAndGetPayload: async input => {
+    const results = await batchPatrols(input);
+    console.log({ results });
+    return results;
+  },
+});
