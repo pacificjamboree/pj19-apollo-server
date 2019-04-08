5d0dfe6b11257cc3154238d55f938676347a24bf
Graham Ballantyne <grahamb@sfu.ca>
2019-04-07 23:31:27 -0700

    Add PATROL_WELCOME to email processor

--- app/queues/processors/sendEmail.js
+++ app/queues/processors/sendEmail.js
@@ -1,10 +1,14 @@
-const { sendPatrolAdventureSelectionMessage } = require('../../lib/mail');
+const {
+  sendPatrolWelcomeMessage,
+  sendPatrolAdventureSelectionMessage,
+} = require('../../lib/mail');
 
 module.exports = () => {
   return async job => {
     const { type, data } = job.data;
-    console.log({ type, data });
     switch (type) {
+      case 'PATROL_WELCOME':
+        return sendPatrolWelcomeMessage(data);
       case 'ADVENTURE_SELECTION':
         return sendPatrolAdventureSelectionMessage(data);
       default:
