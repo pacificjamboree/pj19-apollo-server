fe8c25759f2e9e073c9fabc62470cd4b001bad15
Graham Ballantyne <grahamb@sfu.ca>
2019-05-09 22:22:41 -0700

    Add ability for admin to masquerade as another user

--- app/graphql/mutations/createLoginToken.js
+++ app/graphql/mutations/createLoginToken.js
@@ -1,5 +1,12 @@
 const { mutationWithClientMutationId } = require('graphql-relay-tools');
 const { authenticateUser, generateJWTForUser } = require('../../lib/auth');
+const { EINVALIDCREDENTIALS } = require('../../lib/errors');
+const { User } = require('../../models');
+
+const getUser = username =>
+  User.query()
+    .whereRaw('LOWER(username) = ?', username.toLowerCase())
+    .first();
 
 module.exports = mutationWithClientMutationId({
   name: 'createLoginToken',
@@ -13,13 +20,30 @@ module.exports = mutationWithClientMutationId({
   `,
   mutateAndGetPayload: async ({ username, password }) => {
     try {
-      const user = await authenticateUser(username, password);
-      user.roles = await user.calculateRoles();
-      const token = await generateJWTForUser(user);
-      return {
-        token,
-        viewer: user,
-      };
+      const isMasqueradeLogin = username.includes(':');
+      if (isMasqueradeLogin) {
+        const [subject, superuser] = username.split(':');
+        const user = await authenticateUser(superuser, password);
+        user.roles = await user.calculateRoles();
+        if (!user.roles.includes('admin')) {
+          throw new EINVALIDCREDENTIALS();
+        }
+        const subjectUser = await getUser(subject);
+        if (!subjectUser) throw new EINVALIDCREDENTIALS();
+        const token = await generateJWTForUser(subjectUser);
+        return {
+          token,
+          viewer: subjectUser,
+        };
+      } else {
+        const user = await authenticateUser(username, password);
+        user.roles = await user.calculateRoles();
+        const token = await generateJWTForUser(user);
+        return {
+          token,
+          viewer: user,
+        };
+      }
     } catch (e) {
       throw e;
     }
