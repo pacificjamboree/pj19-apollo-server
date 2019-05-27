ef9de838dc39ab0f83b5aecb7728b7e2c49e6773
Graham Ballantyne <grahamb@sfu.ca>
2019-05-27 13:10:04 -0700

    Add assign_with and child_period columns to Adventure Period

--- db/migrations/20190511013300_CreateAdventurePeriodTable.js
+++ db/migrations/20190511013300_CreateAdventurePeriodTable.js
@@ -7,6 +7,16 @@ exports.up = knex =>
         .primary()
         .default(knex.raw('gen_random_uuid()'));
       t.uuid('adventure_id').references('adventure.id');
+      t.jsonb('assign_with')
+        .defaultTo('[]')
+        .comment(
+          'Assign the patrol to these periods when addind to this one (e.g. for fencing and archery)'
+        );
+      t.jsonb('child_periods')
+        .defaultTo('[]')
+        .comment(
+          'Additional periods to assign with this period for full-day and overnight adventures'
+        );
       t.timestamp('start_at').notNullable();
       t.timestamp('end_at').notNullable();
       t.timestamp('created_at').defaultTo(knex.fn.now());
