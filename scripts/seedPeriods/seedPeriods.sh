e2ed3d55bb490b3bdf059b5d4aaf2777f62d694f
Graham Ballantyne <grahamb@sfu.ca>
2019-05-28 00:01:35 -0700

    call seedArcheryAndStemPeriods in seedPeriods.sh

--- scripts/seedPeriods/seedPeriods.sh
+++ scripts/seedPeriods/seedPeriods.sh
@@ -5,4 +5,5 @@ docker-compose exec web node scripts/seedPeriods/seedHalfDayAdventurePeriods.js
 docker-compose exec web node scripts/seedPeriods/seedFullDayAdventurePeriods.js
 docker-compose exec web node scripts/seedPeriods/seedJdfAdventurePeriods.js
 docker-compose exec web node scripts/seedPeriods/seedFencingAndStemPeriods.js
+docker-compose exec web node scripts/seedPeriods/seedArcheryAndStemPeriods.js
 docker-compose exec web node scripts/seedPeriods/seedOceanWisePeriods.js
