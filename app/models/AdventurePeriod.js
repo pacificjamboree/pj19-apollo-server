534fca10103c8eb7035f1272f0179bb4f76bba7f
Graham Ballantyne <grahamb@sfu.ca>
2019-05-29 14:56:17 -0700

    Add patrols relation to AdventurePeriod

--- app/models/AdventurePeriod.js
+++ app/models/AdventurePeriod.js
@@ -1,5 +1,6 @@
 const Model = require('./BaseModel');
 const Adventure = require('./Adventure');
+const Patrol = require('./Patrol');
 
 class AdventurePeriod extends Model {
   static get tableName() {
@@ -14,6 +15,18 @@ class AdventurePeriod extends Model {
         join: {
           from: 'adventure_period.adventureId',
           to: 'adventure.id',
+        },
+      },
+      patrols: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Patrol,
+        join: {
+          from: 'adventure_period.id',
+          through: {
+            from: 'patrol_schedule.adventurePeriodId',
+            to: 'patrol_schedule.patrolId',
+          },
+          to: 'patrol.id',
         },
       },
     };
