00524c9f560f918e4a401694aac3511a7ff99986
Graham Ballantyne <grahamb@sfu.ca>
2019-06-02 21:52:57 -0700

    Add periodPickers scheduling helper

---
+++ app/lib/scheduling/periodPickers.js
@@ -0,0 +1,30 @@
+const mapBySize = async periods => {
+  const mapped = [];
+  for (const period of periods) {
+    const participantsAssigned = await period.participantsAssigned();
+    mapped.push({ id: period.id, participantsAssigned });
+  }
+  return mapped;
+};
+
+module.exports = {
+  RANDOM: periods => periods[Math.floor(Math.random() * periods.length)],
+  FIRST_IN_TIME: periods =>
+    periods.sort((a, b) => a.startAt.getTime() > b.startAt.getTime())[0],
+  LAST_IN_TIME: periods =>
+    periods.sort((a, b) => a.startAt.getTime() < b.startAt.getTime())[0],
+  MOST_SPACE_AVAILABLE: async periods => {
+    const mapped = await mapBySize(periods);
+    const target = mapped.sort(
+      (a, b) => a.participantsAssigned > b.participantsAssigned
+    )[0];
+    return periods.find(p => p.id === target.id);
+  },
+  LEAST_SPACE_AVAILABLE: async periods => {
+    const mapped = await mapBySize(periods);
+    const target = mapped.sort(
+      (a, b) => a.participantsAssigned < b.participantsAssigned
+    )[0];
+    return periods.find(p => p.id === target.id);
+  },
+};
