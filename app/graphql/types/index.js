f7957eb976e3836936a49d3fd2e3f0ef27d5fb13
Graham Ballantyne <grahamb@sfu.ca>
2019-06-04 21:36:37 -0700

    Use dynamic import/export for types

--- app/graphql/types/index.js
+++ app/graphql/types/index.js
@@ -1,29 +1,10 @@
-const Adventure = require('./Adventure');
-const AdventurePeriod = require('./AdventurePeriod');
-const OfferOfService = require('./OfferOfService');
-const OfferOfServiceCount = require('./OfferOfServiceCount');
-const OfferOfServiceNode = require('./OfferOfServiceNode');
-const OffersOfServiceConnection = require('./OffersOfServiceConnection');
-const Patrol = require('./Patrol');
-const PatrolScouter = require('./PatrolScouter');
-const PatrolAdventureSelection = require('./PatrolAdventureSelection');
-const PatrolAdventureSelectionStats = require('./PatrolAdventureSelectionStats');
-const PatrolStats = require('./PatrolStats');
-const TextContent = require('./TextContent');
-const User = require('./User');
+const fs = require('fs');
+const types = {};
+fs.readdirSync(__dirname)
+  .filter(f => f !== 'index.js')
+  .map(f => f.replace('.js', ''))
+  .forEach(f => {
+    types[f] = require(`./${f}`);
+  });
 
-module.exports = {
-  Adventure,
-  AdventurePeriod,
-  OfferOfService,
-  OfferOfServiceCount,
-  OfferOfServiceNode,
-  OffersOfServiceConnection,
-  Patrol,
-  PatrolScouter,
-  PatrolAdventureSelection,
-  PatrolAdventureSelectionStats,
-  PatrolStats,
-  TextContent,
-  User,
-};
+module.exports = types;
