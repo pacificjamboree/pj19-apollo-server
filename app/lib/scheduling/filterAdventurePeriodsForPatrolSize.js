ca7626df10711133a03edda5869b24751e1a67ed
Graham Ballantyne <grahamb@sfu.ca>
2019-06-07 20:41:23 -0700

    Fix filterAdventurePEriodsForPatrolSize

--- app/lib/scheduling/filterAdventurePeriodsForPatrolSize.js
+++ app/lib/scheduling/filterAdventurePeriodsForPatrolSize.js
@@ -2,7 +2,9 @@ const filterAdventurePeriodsForPatrolSize = async (periods, patrolSize) => {
   const periodsWithCapacity = [];
   for (const period of periods) {
     const assigned = await period.participantsAssigned();
-    if (assigned.total <= patrolSize) {
+    const adventure = await period.$relatedQuery('adventure');
+    const roomLeft = adventure.capacityPerPeriod - assigned.total;
+    if (roomLeft >= patrolSize) {
       periodsWithCapacity.push(period);
     }
   }
