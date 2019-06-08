850b685eca778fef9b3221a09b841735d229e3b2
Graham Ballantyne <grahamb@sfu.ca>
2019-06-07 20:42:41 -0700

    findPeriodForAdventure fixes

--- app/lib/scheduling/findPeriodForAdventure.js
+++ app/lib/scheduling/findPeriodForAdventure.js
@@ -1,3 +1,4 @@
+const isWithinRange = require('date-fns/is_within_range');
 const vacantPeriodsForPatrol = require('./vacantPeriodsForPatrol');
 const {
   halfDayOffSite,
@@ -21,11 +22,21 @@ const findPeriodForAdventure = async (adventure, patrol, how = 'RANDOM') => {
   try {
     const { periodsRequired, location } = adventure;
     const schedule = await patrol.$relatedQuery('schedule');
-    const periods = await adventure.$relatedQuery('periods');
+    const periods = await adventure
+      .$relatedQuery('periods')
+      .where({ type: 'primary' });
     // get vacant periods for the patrol
     const vacant = await vacantPeriodsForPatrol(patrol);
-    const vacantAdventurePeriods = periods.filter(p =>
-      vacant.find(pp => pp.startAt.getTime() === p.startAt.getTime())
+    const vacantAdventurePeriods = periods.filter(period =>
+      vacant.find(
+        vacantPeriod =>
+          isWithinRange(
+            period.startAt,
+            vacantPeriod.startAt,
+            vacantPeriod.endAt
+          )
+        // vacantPeriod.startAt.getTime() === period.startAt.getTime()
+      )
     );
 
     // get all available periods for this type of adventure
@@ -34,16 +45,16 @@ const findPeriodForAdventure = async (adventure, patrol, how = 'RANDOM') => {
       case 1:
         potentialPeriods =
           location === 'onsite'
-            ? await halfDayOnSite(vacantAdventurePeriods)
+            ? await halfDayOnSite(vacantAdventurePeriods, schedule)
             : await halfDayOffSite(vacantAdventurePeriods, schedule);
         break;
 
       case 2:
-        potentialPeriods = await fullDay(vacantAdventurePeriods);
+        potentialPeriods = await fullDay(vacantAdventurePeriods, schedule);
         break;
 
       case 3:
-        potentialPeriods = await overnight(vacantAdventurePeriods);
+        potentialPeriods = await overnight(vacantAdventurePeriods, schedule);
         break;
 
       default:
@@ -51,12 +62,13 @@ const findPeriodForAdventure = async (adventure, patrol, how = 'RANDOM') => {
     }
 
     // narrow down the list to periods that fit the patrol
-    const size = patrol.totalUnitSize();
-    const periodsThatFitPatrol = await filterAdventurePeriodsForPatrolSize(
-      potentialPeriods,
-      size
-    );
+    const size = patrol.numberOfScouts + 2; // always only two scouters
+    const periodsThatFitPatrol =
+      adventure.adventureCode === 'free'
+        ? potentialPeriods
+        : await filterAdventurePeriodsForPatrolSize(potentialPeriods, size);
     const period = await periodPickers[how](periodsThatFitPatrol);
+
     return period;
   } catch (error) {
     throw error;
